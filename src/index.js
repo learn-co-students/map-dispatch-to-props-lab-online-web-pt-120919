8196e7be8731593bde87840df1feef91c5407016
Mely07 <damely.tineo07@gmail.com>
2020-09-17 19:10:10 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import manageRestaurants from './reducers/manageRestaurants';
+import { Provider } from 'react-redux';
 
 const store = createStore(manageRestaurants, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
